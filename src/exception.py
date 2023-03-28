8ded62aa36d75fa7998451ffa6d7dd5a95a5cefd
tapabratidas <tapabratidas@gmail.com>
2023-03-28 20:48:25 +0530

    adding exception and loggers

---
+++ src/exception.py
@@ -0,0 +1,24 @@
+import sys
+from loggers import logging
+
+def error_message_detail(error, error_detail:sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    lineno = exc_tb.tb_lineno
+    error_message = "error occured in python script name [{0}] line no [{1}] error message [{2}]".format(file_name,lineno,str(error))
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        a = 45/0
+    except Exception as e:
+        logging.info("divide by zero")
+        raise CustomException(e,sys)
---
+++ src/loggers.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from datetime import datetime
+
+log_file = f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(),"logs",log_file)
+os.makedirs(logs_path,exist_ok=True)
+
+log_file_path = os.path.join(logs_path,log_file)
+
+logging.basicConfig(
+    filename = log_file_path,
+    format = "%(line_no)s - %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO
+
+)
+
+if __name__ == "__main__":
+    logging.info("logging has started")
