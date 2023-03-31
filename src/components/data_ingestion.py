4839ec887145339571c6cd342353227cca12053d
tapabratidas <tapabratidas@gmail.com>
2023-03-31 19:00:09 +0530

    added data transformation code

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,7 @@ from src.loggers import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -37,4 +38,7 @@ class DataIngestion:
         
 if __name__ ==  "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data, = obj.initiate_data_ingestion()
+    print(train_data,test_data)
+    data_transform = DataTransformation()
+    data_transform.initiate_data_transformation(train_data,test_data)
